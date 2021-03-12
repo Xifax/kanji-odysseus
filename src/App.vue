629732a92099cab75dc0e1307d29bcdf96833739
Artem_Basenko <Artem_Basenko@epam.com>
2021-03-12 20:59:04 +0300

    Include bulma and apply some kind of simple theme

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <HelloKanji msg="Random kanji" />
+  <HelloKanji />
 </template>
 
 <script>
-import HelloKanji from './components/HelloKanji.vue'
+import HelloKanji from "./components/HelloKanji.vue";
+// TODO: include https://docs.netlify.com/site-deploys/overview/#deploy-contexts
+// TODO: include jsconfig.json https://vuejs.github.io/vetur/guide/FAQ.html#how-to-build-and-install-from-source
+// TODO: configure prettier and so on
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloKanji
-  }
-}
+    HelloKanji,
+  },
+};
 </script>
+
+<style lang="sass">
+@import "./bulma/bulma.sass"
+@import "theme.scss"
+</style>
--- src/components/HelloKanji.vue
+++ src/components/HelloKanji.vue
@@ -1,64 +1,105 @@
 <template>
-  <h2>{{ msg }}</h2>
-  <h1>{{ kanji.kanji }}</h1>
-  <!-- TODO: apply cool styles for everything -->
-  <a v-for="k in kanji.group"
-    :key="k"
-    href="#"
-    @click="setKanji(k)">{{ k }}&nbsp;</a>
-    <!-- TODO: get meaning from odyssey? -->
-  <p>{{ kanji.on }}</p>
-  <p>{{ kanji.kun }}</p>
-  <!-- TODO: make responsive? -->
-  <div v-for="w in kanji.words" 
-    :key="w">
-    <!-- Highlight kanji in word -->
-    <div v-html="highlight(w.kanji)"></div>
-    {{ w.reading }}
-    {{ w.meaning }}
+  <div class="container">
+    <!-- TODO: customize kanji rank (e.g., only id > 1000) -->
+    <!-- TODO: button ROLL new kanji -->
+    <section class="hero is-small is-link box">
+      <div class="hero-body">
+        <h1 class="title kanji-title">{{ kanji.kanji }}</h1>
+      </div>
+    </section>
+    <!-- TODO: apply cool styles for everything -->
+    <div class="columns is-centered box">
+        <div class="field is-grouped">
+          <p
+            class="control"
+            v-for="k in kanji.group"
+            :key="k"
+            href="#"
+            @click="setKanji(k)"
+          >
+            <button class="button is-medium is-outlined is-link">
+              {{ k }}
+            </button>
+          </p>
+        </div>
     </div>
+
+    <!-- TODO: get meaning from odyssey? -->
+    <!-- TODO: check some readings: TAMA, ATAI, ― -->
+    <div class="container block">
+      <span class="tag is-light is-info is-large">{{ kanji.on }}</span>
+      <span class="tag is-light is-primary is-large">{{ kanji.kun }}</span>
+    </div>
+
+    <!-- Highlight kanji in word -->
+    <!-- TODO: make KANJI ONLY clickable -->
+    <div class="columns is-centered">
+      <table class="table">
+        <tr v-for="w in kanji.words" :key="w">
+          <td v-html="highlight(w.kanji)"></td>
+          <td>{{ w.reading }}</td>
+          <td>{{ w.meaning }}</td>
+        </tr>
+      </table>
+
+      <!-- <div v-for="w in kanji.words" :key="w">
+        <span v-html="highlight(w.kanji)"></span>
+        {{ w.reading }}
+        {{ w.meaning }}
+      </div> -->
+    </div>
+  </div>
 </template>
 
 <script>
-import kanjiData from '../assets/kanji.json'
+import kanjiData from "../assets/kanji.json";
 export default {
-  name: 'HelloKanji',
-  props: {
-    msg: String
-  },
+  name: "HelloKanji",
+  props: {},
   mounted() {
-    this.setRandomKanji()
+    this.setRandomKanji();
   },
   data() {
     return {
       // Current kanji
-      kanji: ''
-    }
+      kanji: "",
+    };
   },
   methods: {
     // Set kanji by key
     setKanji(kanji) {
-      this.kanji = kanjiData[kanji]
+      this.kanji = kanjiData[kanji];
     },
     // Get random kanji key
     getRandomKanji() {
-      let availableKanji = Object.keys(kanjiData)
-      return availableKanji[Math.floor(Math.random() * availableKanji.length)]
+      let availableKanji = Object.keys(kanjiData);
+      return availableKanji[Math.floor(Math.random() * availableKanji.length)];
     },
     // Update current kanji
     setRandomKanji() {
-      this.kanji = kanjiData[this.getRandomKanji()]
+      this.kanji = kanjiData[this.getRandomKanji()];
     },
     // Replace current kanji with some custom formatting
     highlight(word) {
-      return word.replace(this.kanji.kanji, `<strong>${this.kanji.kanji}</strong>`)
+      return word.replace(
+        this.kanji.kanji,
+        `<strong>${this.kanji.kanji}</strong>`
+      );
+    },
+    // Make kanji clickable
+    referenceKanji(word) {
+      // TODO: scan each character and wrap it with @click="setKanji(?)" ???
+    },
+    // Check if character is kanji
+    isKanji(char) {
+      return /[\u4e00-\u9faf\u3400-\u4dbf]/.test(char);
     },
   },
   computed: {
     // Separate property for current kanji
     randomKanji() {
-      return kanjiData[this.getRandomKanji()]
-    }
-  }
-}
+      return kanjiData[this.getRandomKanji()];
+    },
+  },
+};
 </script>
